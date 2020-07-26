1d778f1f2c80ef55c3c587b51b8306a58f467ad7
riya-agarwal-71 <riya.ec19@nsut.ac.in>
2020-07-26 22:02:21 +0530

    ball movement added

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// max left paddle (vw - paddlewidth)
-// min left paddle 0
-// max left ball (vw - ballwidth)
-// min left ball 0
-// min top ball 0
-// max top ball (vh - ballheight)
-// top ball collision with upper paddle (paddlewidth)
-// top ball collision with lower paddle (vh - paddlewidth - ballwidth)
 (function(){
     var p1 = document.getElementById("one");
     var p2 = document.getElementById("two");
@@ -18,7 +10,7 @@
         return gamearea.offsetWidth - ball.offsetWidth;
     }
     function maxtopforball(){
-        return gamearea.offsetHeight - ball.offsetHeight;
+        return gamearea.offsetHeight - ball.offsetHeight - p1.offsetHeight;
     }
     function topcollupper(){
         return p1.offsetHeight;
@@ -26,31 +18,29 @@
     function topcolllower(){
         return gamearea.offsetHeight - p1.offsetHeight - ball.offsetWidth;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function moveball(){
+        var leftchange = 2;
+        var topchange = 2;
+        var leftnow = ball.getBoundingClientRect().left - gamearea.getBoundingClientRect().left;
+        var topnow = ball.getBoundingClientRect().top - gamearea.getBoundingClientRect().top;
+        var int = setInterval(function(){
+            if(leftnow + leftchange < maxleftforball() && leftnow + leftchange > 0){
+                leftnow = leftnow + leftchange;
+                ball.style.left = leftnow + 'px';
+            }else{
+                leftchange = -leftchange;
+                leftnow = leftnow + leftchange;
+                ball.style.left = leftnow + 'px';
+            }
+            if(topnow + topchange < maxtopforball() && topnow + topchange > p1.offsetHeight){
+                topnow = topnow + topchange;
+                ball.style.top = topnow + 'px';
+            }else{
+                topchange = -topchange;
+                topnow = topnow + topchange;
+                ball.style.top = topnow + 'px';
+            }
+        },8)
+    }
+    moveball();
 })()
